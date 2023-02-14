0aba1b0c788e1f5a2c6e2debf430c75cba2c3a4e
AKASH KUMAR <71625383+Akashk21@users.noreply.github.com>
2023-02-14 15:13:13 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, Akash\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built and run HELLO TO THE CC LAB\n";
   return 0;
 }
